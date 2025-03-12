883339131300d31a254472192a1d182006ec6f47
akshitsharma004 <akshitsharma9729@gmail.com>
2025-03-12 22:33:00 +0530

    done

--- src/Components/HomeSecond.jsx
+++ src/Components/HomeSecond.jsx
@@ -13,23 +13,23 @@ function HomeSecond() {
         { heading: "🌱 Sustainable", para: "Reduce paper waste by giving books a second life. Every exchange supports a greener planet." },
         { heading: "🚀 Unlimited Reads", para: "With BookSwap, you’ll never run out of books to read! Swap as often as you want." },
     ];
-    useGSAP(() => {
-        gsap.to("#slide1", {
-            x: "20%",
-            duration: 3,
-            repeat: 1,
-            stagger: 1,
-            ease: "none",
-        });
-    });
-    useGSAP(() => {
-        gsap.from("#slide2", {
-            x: "-100%",
-            duration: 3,
-            repeat: 1,
-            ease: "none",
-        });
-    });
+    // useGSAP(() => {
+    //     gsap.to("#slide1", {
+    //         x: "20%",
+    //         duration: 3,
+    //         repeat: 1,
+    //         stagger: 1,
+    //         ease: "none",
+    //     });
+    // });
+    // useGSAP(() => {
+    //     gsap.from("#slide2", {
+    //         x: "-100%",
+    //         duration: 3,
+    //         repeat: 1,
+    //         ease: "none",
+    //     });
+    // });
     return (
         <div className="bg-black overflow-hidden py-28 text-white flex gap-28">
             <div>
